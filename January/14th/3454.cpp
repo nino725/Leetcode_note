9830c074ac7bf5e7cbfef985f746bcc2c3650fae
wjx13753607566@163.com <227969083+nino725@users.noreply.github.com>
2026-01-14 13:24:14 +0800

    This question is too difficult. Remember to study it later

--- January/14th/3454.cpp
+++ January/14th/3454.cpp
@@ -18,10 +18,136 @@ int main() {
 
 注意：正方形 可能会 重叠。重叠区域只 统计一次 。
 */
+class SegmentTree {
+public:
+    SegmentTree(vector<int>& xs) {
+        unsigned n = xs.size() - 1; // xs.size() 个横坐标有 xs.size()-1 个差值
+        seg.resize(2 << bit_width(n - 1));
+        build(xs, 1, 0, n - 1);
+    }
 
+    void update(int l, int r, int v) {
+        update(1, l, r, v);
+    }
+
+    int get_uncovered_length() {
+        return seg[1].min_cover ? 0 : seg[1].min_cover_len;
+    }
+
+private:
+    struct Node {
+        int l, r;
+        int min_cover_len = 0; // 区间内被矩形覆盖次数最少的底边长之和
+        int min_cover = 0;     // 区间内被矩形覆盖的最小次数
+        int todo = 0;          // 子树内的所有节点的 min_cover 需要增加的量，注意这可以是负数
+    };
+
+    vector<Node> seg;
+
+    // 根据左右儿子的信息，更新当前节点的信息
+    void maintain(int o) {
+        Node& lo = seg[o * 2];
+        Node& ro = seg[o * 2 + 1];
+        int mn = min(lo.min_cover, ro.min_cover);
+        seg[o].min_cover = mn;
+        // 只统计等于 min_cover 的底边长之和
+        seg[o].min_cover_len = (lo.min_cover == mn ? lo.min_cover_len : 0) +
+                               (ro.min_cover == mn ? ro.min_cover_len : 0);
+    }
+
+    // 仅更新节点信息，不下传懒标记 todo
+    void do_(int o, int v) {
+        seg[o].min_cover += v;
+        seg[o].todo += v;
+    }
+
+    // 下传懒标记 todo
+    void spread(int o) {
+        int& v = seg[o].todo;
+        if (v != 0) {
+            do_(o * 2, v);
+            do_(o * 2 + 1, v);
+            v = 0;
+        }
+    }
+
+    // 建树
+    void build(vector<int>& xs, int o, int l, int r) {
+        seg[o].l = l;
+        seg[o].r = r;
+        if (l == r) {
+            seg[o].min_cover_len = xs[l + 1] - xs[l];
+            return;
+        }
+        int m = (l + r) / 2;
+        build(xs, o * 2, l, m);
+        build(xs, o * 2 + 1, m + 1, r);
+        maintain(o);
+    }
+
+    // 区间更新
+    void update(int o, int l, int r, int v) {
+        if (l <= seg[o].l && seg[o].r <= r) {
+            do_(o, v);
+            return;
+        }
+        spread(o);
+        int m = (seg[o].l + seg[o].r) / 2;
+        if (l <= m) {
+            update(o * 2, l, r, v);
+        }
+        if (m < r) {
+            update(o * 2 + 1, l, r, v);
+        }
+        maintain(o);
+    }
+};
+
+// 代码逻辑同 850 题，增加一个 records 数组记录关键数据
 class Solution {
 public:
     double separateSquares(vector<vector<int>>& squares) {
-        
+        vector<int> xs;
+        struct Event { int y, lx, rx, delta; };
+        vector<Event> events;
+        for (auto& sq : squares) {
+            int lx = sq[0], y = sq[1], l = sq[2];
+            int rx = lx + l;
+            xs.push_back(lx);
+            xs.push_back(rx);
+            events.emplace_back(y, lx, rx, 1);
+            events.emplace_back(y + l, lx, rx, -1);
+        }
+
+        // 排序去重，方便离散化
+        ranges::sort(xs);
+        xs.erase(ranges::unique(xs).begin(), xs.end());
+
+        // 初始化线段树
+        SegmentTree t(xs);
+
+        // 模拟扫描线从下往上移动
+        ranges::sort(events, {}, &Event::y);
+        vector<pair<long long, int>> records(events.size() - 1);
+        long long tot_area = 0;
+        for (int i = 0; i + 1 < events.size(); i++) {
+            auto& [y, lx, rx, delta] = events[i];
+            int l = ranges::lower_bound(xs, lx) - xs.begin(); // 离散化
+            int r = ranges::lower_bound(xs, rx) - xs.begin() - 1; // r 对应着 xs[r] 与 xs[r+1]=rx 的差值
+            t.update(l, r, delta); // 更新被 [lx, rx] 覆盖的次数
+            int sum_len = xs.back() - xs[0] - t.get_uncovered_length(); // 减去没被矩形覆盖的长度
+            records[i] = {tot_area, sum_len};
+            tot_area += 1LL * sum_len * (events[i + 1].y - y); // 新增面积 = 被至少一个矩形覆盖的底边长之和 * 矩形高度
+        }
+
+        // 二分找最后一个 < tot_area / 2 的面积
+        int i = ranges::lower_bound(records, tot_area, {}, [](auto& p) { return p.first * 2; }) - records.begin() - 1;
+        auto [area, sum_len] = records[i];
+        return events[i].y + (tot_area - area * 2) / (sum_len * 2.0);
     }
 };
+
+/*作者：灵茶山艾府
+链接：https://leetcode.cn/problems/separate-squares-ii/solutions/3078402/lazy-xian-duan-shu-sao-miao-xian-pythonj-eeqk/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。*/
