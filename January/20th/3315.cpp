e129f05035e05960ced6abb04d428eba691c36ce
wjx13753607566@163.com <227969083+nino725@users.noreply.github.com>
2026-01-20 19:16:58 +0800

    nothing

---
+++ January/20th/3315.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    
+    return 0;
+}
+
+/*
+给你一个长度为 n 的质数数组 nums 。你的任务是返回一个长度为 n 的数组 ans ，对于每个下标 i ，以下 条件 均成立：
+
+ans[i] OR (ans[i] + 1) == nums[i]
+除此以外，你需要 最小化 结果数组里每一个 ans[i] 。
+
+如果没法找到符合 条件 的 ans[i] ，那么 ans[i] = -1 。
+
+质数 指的是一个大于 1 的自然数，且它只有 1 和自己两个因数。
+*/
+
